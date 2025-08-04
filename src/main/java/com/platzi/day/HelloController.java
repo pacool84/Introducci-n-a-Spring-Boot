0fdf74221d1ebae9d34b60182b86c5f4d21e48f4
Pacool84 <flopez_1384@hotmail.com>
2025-08-03 20:55:18 -0600

    [ADD] archivos de configuracion application.properties

--- src/main/java/com/platzi/day/HelloController.java
+++ src/main/java/com/platzi/day/HelloController.java
@@ -1,19 +1,23 @@
 package com.platzi.day;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //Indica que es un controlador de peticiones
 public class HelloController {
+    private final String platform;
     private final PlatziPlayAIService aiService;
+
     // Constructor que inyecta el servicio de IA
-    public HelloController(PlatziPlayAIService aiService) {
+    public HelloController(@Value("${spring.application.name}") String platform, PlatziPlayAIService aiService) {
+        this.platform = platform;
         this.aiService = aiService;
     }
 
     // Método que maneja las peticiones GET a la ruta /
     @GetMapping("/") //Indica que responde a peticiones GET en la ruta /
     public String hello() {
-        return this.aiService.generateGreeting(); // Llama al servicio de IA para generar un saludo
+        return this.aiService.generateGreeting(platform); // Llama al servicio de IA para generar un saludo
     }
 }
--- src/main/java/com/platzi/day/PlatziPlayAIService.java
+++ src/main/java/com/platzi/day/PlatziPlayAIService.java
@@ -1,16 +1,17 @@
 package com.platzi.day;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService// Anotación que indica que es un servicio de IA
 public interface PlatziPlayAIService {
 
     @UserMessage("""
-            Genera una saludo de bienvenida a la plataforma de Gestion de Peliculas Platzi Play.
+            Genera una saludo de bienvenida a la plataforma de Gestion de Peliculas {{platform}}.
             Usa menos de 120 caracteres hazlo con el estilo de Platzi.
             """)
-    String generateGreeting();
+    String generateGreeting(@V("platform") String platform); // Método que genera un saludo de bienvenida para la plataforma
 }
 // Esta interfaz define un servicio de IA que genera un saludo de bienvenida
 // para la plataforma de gestión de películas Platzi Play.
