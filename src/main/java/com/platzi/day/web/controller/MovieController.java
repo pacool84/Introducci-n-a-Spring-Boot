64e8e3e441358366a0a7a534f6fce50da53b27f7
Pacool84 <flopez_1384@hotmail.com>
2025-08-23 10:11:37 -0600

    [ADD] ResponseEntity para manejo de respuestas en los endpoint actuales

--- src/main/java/com/platzi/day/web/controller/MovieController.java
+++ src/main/java/com/platzi/day/web/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.platzi.day.web.controller;
 
 import com.platzi.day.domain.dto.MovieDto;
 import com.platzi.day.domain.service.MovieService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +21,19 @@ public class MovieController {
 
     // Endpoint para obtener todas las peliculas
     @GetMapping// Indica que responde a peticiones GET en la ruta /movies
-    public List<MovieDto> getAllMovies() {
-        return this.movieService.getAllMovies();
+    public ResponseEntity<List<MovieDto>> getAllMovies() {
+        return ResponseEntity.ok(this.movieService.getAllMovies()); // Devuelve 200 OK con la lista de peliculas
     }
 
     // Endpoint para obtener una pelicula por su ID
     @GetMapping("/{id}") // Indica que responde a peticiones GET en la ruta /movies/{id}
-    public MovieDto getById(@PathVariable long id) {
-        return this.movieService.getById(id);
+    public ResponseEntity<MovieDto> getById(@PathVariable long id) {
+        MovieDto movieDto = this.movieService.getById(id);
+
+        if (movieDto != null) {
+            return ResponseEntity.ok(movieDto); // Devuelve 200 OK con la pelicula encontrada
+        } else {
+            return ResponseEntity.notFound().build(); // Devuelve 404 Not Found si no se encuentra
+        }
     }
 }
